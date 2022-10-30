e88fc61583b45eed9e7c112e36d1769c0b83134f
Beyza Kılıç <beyza7259@gmail.com>
2022-10-30 07:17:58 +0300

    API

---
+++ src/test/java/get_requests/Get06b.java
@@ -0,0 +1,72 @@
+package get_requests;
+import base_url.ReqresBaseUrl;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.Test;
+import java.util.List;
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
+
+public class Get06b extends ReqresBaseUrl {
+     /*
+       Given
+              https://reqres.in/api/unknown/
+       When
+            I send GET Request to the URL
+       Then
+
+            1)Status code is 200
+            2)Print all pantone_values
+            3)Print all ids greater than 3 on the console
+              Assert that there are 3 ids greater than 3
+            4)Print all names whose ids are less than 3 on the console
+              Assert that the number of names whose ids are less than 3 is 2
+    */
+
+    @Test
+    public void get06(){
+
+        //Set the Url
+        spec.pathParam("first","unknown");
+
+        //Set The Expected Data
+
+        //Send The Request and Get The Response
+        Response response = given().spec(spec).when().get("/{first}");
+        response.prettyPrint();
+
+        //Do Assertion
+        JsonPath jsonPath = response.jsonPath();
+
+        //1)Status code is 200
+        assertEquals(200,response.getStatusCode());
+
+        //2)Print all pantone_values
+        System.out.println(jsonPath.getList("data.pantone_value")); //.get(0) desem sonuna, 0. index olanı verir
+
+        //3)Print all ids greater than 3 on the console
+        List<Integer> ids = jsonPath.getList("data.findAll{it.id>3}.id");
+        System.out.println("ids = " + ids);
+        //id'si 3'ten buyuk olanların arasından id'leri getir dedim,
+        //id yerine icerisinden herhangi baska bir sey de yazabilirdim
+
+        //Assert that there are 3 ids greater than 3
+        assertEquals(3,ids.size());
+        //ids.size() ---> id'lerin uzunlugu 3'e esitse
+
+       //4)Print all names whose ids are less than 3 on the console
+        List<String> names = jsonPath.getList("data.findAll{it.id<3}.name");
+        System.out.println("names = " + names);
+        //data'dan sonra collection yani list var diye,
+        //data.findAll(){it.id<3} ---> yaptık, it de her bir element demek
+
+       //Assert that the number of names whose ids are less than 3 is 2
+        assertEquals(2,names.size());
+
+    }
+}
+
+
+
+
+
